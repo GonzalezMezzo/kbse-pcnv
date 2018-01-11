e6425b2adbab6fc15c76d1083c9ac60254122940
p.nienhueser <philipp.nienhueser@hs-osnabrueck.de>
2018-01-11 17:34:41 +0100

    entity classes first commit

--- src/java/access/PostDTO.java
+++ src/java/access/PostDTO.java
@@ -5,14 +5,68 @@
  */
 package access;
 
+import java.io.Serializable;
+
 /**
  *
  * @author philippnienhuser
  */
-public class PostDTO {
+public class PostDTO implements Serializable{
     
-    long id;
+    private long id;
+    private String url;
+    private String comment;
+    private String creator;
+    private int rating;
+
+    public PostDTO(){}
+
+    public PostDTO(String url, String comment, String creator, int rating) {
+        this.url = url;
+        this.comment = comment;
+        this.creator = creator;
+        this.rating = rating;
+    }
     
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public String getCreator() {
+        return creator;
+    }
+
+    public void setCreator(String creator) {
+        this.creator = creator;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
     
     
 }
--- src/java/enitities/Post.java
+++ src/java/enitities/Post.java
@@ -6,10 +6,12 @@
 package enitities;
 
 import java.io.Serializable;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Version;
 
 /**
  *
@@ -21,7 +23,28 @@ public class Post implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="id",
+            nullable=false,
+            unique=true)
     private Long id;
+    @Column(name="url",
+            nullable=false,
+            unique=true)
+    private String url;
+    @Column(name="comment",
+            nullable=true,
+            unique=false)
+    private String comment;
+    @Column(name="creator",
+            nullable=false,
+            unique=false)
+    private String creator;
+    @Column(name="rating",
+            nullable=true,
+            unique=false)
+    private int rating;
+    @Version
+    private int version;
 
     public Long getId() {
         return id;
@@ -31,6 +54,38 @@ public class Post implements Serializable {
         this.id = id;
     }
 
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public String getCreator() {
+        return creator;
+    }
+
+    public void setCreator(String creator) {
+        this.creator = creator;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+    
     @Override
     public int hashCode() {
         int hash = 0;
