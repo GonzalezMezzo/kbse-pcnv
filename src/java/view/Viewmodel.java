cecead6efbb1e472a9a0d72f73add1c0a96032f7
pnienhue <pnienhue@SI0203-01.intra.edvsz>
2018-02-05 13:11:06 +0100

    rest

--- src/java/view/Viewmodel.java
+++ src/java/view/Viewmodel.java
@@ -25,14 +25,14 @@ import javax.inject.Named;
 @Named(value = "viewmodel")
 @SessionScoped
 public class Viewmodel implements Serializable{
-    /*
+
     @Inject
     RestFrontendController rfctrl;
-    */
-    
+
+    /*
     @Inject
     ModelController mdlctrl;
-    
+    */
     private static final String INDEX = "/index.xhtml?faces-redirect=true";
     private static final String RATING = "/rating.xhtml?faces-redirect=true";
     
@@ -82,26 +82,26 @@ public class Viewmodel implements Serializable{
         PostDTO post = new PostDTO(url,comment,inputTextUser,0,new HashMap<String,Integer>(), new ArrayList<CommentDTO>());
         post.getComments().add(new CommentDTO(comment, inputTextUser));
         post.getRatings().put(inputTextUser, 0);
-        mdlctrl.addPost(post);
+        rfctrl.addPost(post);
         refreshState();
         return INDEX;
     }
     
     public String delete(PostDTO p){
-        mdlctrl.deletePost(p.getId());
+        rfctrl.deletePost(p.getId());
         refreshState();
         return INDEX;
     }
     
     public String submitComment(){
         CommentDTO comment = new CommentDTO(inputTextComment, inputTextUser);
-        mdlctrl.addComment(currentPostId, comment);
+        rfctrl.addComment(currentPostId, comment);
         refreshState();
         return INDEX;
     }
     
     public void refreshState(){
-        this.postList = mdlctrl.refreshState();
+        this.postList = rfctrl.refreshState();
         ratingCollector = new int[postList.size()];
         inputTextNumber = 0;
     }
@@ -127,11 +127,10 @@ public class Viewmodel implements Serializable{
         for(int i =0;i<ratingCollector.length;i++){
             if(ratingCollector[i]!=0)
                 this.postList.get(i).getRatings().put(inputTextUser, new Integer(ratingCollector[i]));
-            mdlctrl.updateRatings(this.postList);
+            rfctrl.updateRatings(this.postList);
         }
         refreshState();
         }
-        
     }
     
     /**
